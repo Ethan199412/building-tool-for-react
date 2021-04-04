e21eead1c6b59a36e70fe6ea1dcfb4f2e50fede7
Ethan199412 <lijiahao1213ok@163.com>
2021-04-04 21:53:50 +0800

    'v1.0'

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component} from "react";
+import "./index.less";
+import img from './react.jpg'
+
+class App extends Component{
+    constructor(props){
+        super(props)
+    }
+
+  render(){
+    return(
+      <div className="App">
+        <img src={img} style={{width:100}}/>
+        <h1> This is a building Tool designed by Ethan, Welcome for using </h1>
+        <div> compared with create-react-app, this building tool will export webpack.config.js so that you could customize for your project.</div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test:/\.(png|jpg|jpeg)$/,
+        use:['file-loader']
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test:/\.less$/,
+        use:['style-loader','css-loader','less-loader']
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 3001,
+    publicPath: "http://localhost:3001/dist/",
+    hot: true
+  },
+  plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      
+    ]
+};

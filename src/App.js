dd9018e3ed34d4f1f158777307a5d1d091e45cc9
Ethan199412 <lijiahao1213ok@163.com>
2021-05-01 01:57:19 +0800

    text

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <img src={img} style={{ width: 100 }} />
         <h1> This is a building Tool designed by Ethan, Welcome for using. </h1>
         <div>
-          compared with create-react-app, this building tool will export
+          compared with create-react-app, this building tool will expose
           webpack.config.js so that you could customize for your project.
         </div>
       </div>

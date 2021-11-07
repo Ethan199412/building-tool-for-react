f5f87dda78bd40821fd244d346cad6e72cdfc484
lijiahao043 <lijiahao043@ke.com>
2021-11-07 21:14:21 +0800

    flow

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+// @flow
 import React, { Component } from "react";
+
+type f1 = (a: number, b: number) => number
+
 import "./index.less";
 import img from "./react.jpg";
 
-class App extends Component {
-  constructor(props) {
+class App extends Component<any> {
+  constructor(props: any) {
     super(props);
   }
 
-  render() {
+  handleClick: Function = () => {
+    console.log(this.add(1, 4))
+  }
+
+  add: f1 = (a, b) => {
+    return a + b
+  }
+
+  render(): any {
     return (
       <div className="App">
         <img src={img} style={{ width: 100 }} />
@@ -16,6 +28,7 @@ class App extends Component {
           compared with create-react-app, this building tool will expose
           webpack.config.js so that you could customize for your project.
         </div>
+        <button onClick={this.handleClick}>click</button>
       </div>
     );
   }

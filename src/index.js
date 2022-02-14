af89dac549906cf55e94be763332e2766ce8f8a2
Ethan199412 <lijiahao1213ok@163.com>
2022-02-15 01:19:51 +0800

    pwa

--- src/index.js
+++ src/index.js
@@ -4,4 +4,15 @@ import App from "./ts/app";
 
 console.log('index.js 加载')
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load',
+        () => {
+            navigator.serviceWorker.register('/dist/service-worker.js').then(_ => {
+                console.log('sw 注册成功')
+            }).catch(_=>{
+                console.log('sw 注册失败')
+            })
+        })
+}
+
 ReactDOM.render(<App />, document.getElementById("root"));

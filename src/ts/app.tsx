18196951f0ec51e486aaf0d5fedf346b0277c4f0
lijiahao <lijiahao@shopee.com>
2022-10-20 19:35:26 +0800

    flex

--- src/ts/app.tsx
+++ src/ts/app.tsx
@@ -1,41 +1,75 @@
 import * as React from "react";
 import "./app.less";
-import { mult, pow } from '../utils'
+import { mult, pow } from "../utils";
+import { Bar } from "@ant-design/plots";
 
-console.log('[p1]', mult(2, 3))
 // 需要放 index.d.ts 文件到根目录并在 tsconfig
 // includes 里引入才能解决 vscode 报错
-import img from "../assets/react.jpg";
+// import img from "../assets/react.jpg";
 
-console.log('app.tsx 加载')
-if (module.hot) {
-    module.hot.accept('./app.tsx', () => {
-        console.log('[p0] hot module replacement')
-    })
-}
-
+console.log("app.tsx 加载");
+// if (module.hot) {
+//     module.hot.accept('./app.tsx', () => {
+//         console.log('[p0] hot module replacement')
+//     })
+// }
+const DemoBar = () => {
+  const data = [
+    {
+      year: "1951 年",
+      value: 38,
+    },
+    {
+      year: "1952 年",
+      value: 52,
+    },
+    {
+      year: "1956 年",
+      value: 61,
+    },
+    {
+      year: "1957 年",
+      value: 145,
+    },
+    {
+      year: "1958 年",
+      value: 48,
+    },
+  ];
+  const config = {
+    data,
+    xField: "value",
+    yField: "year",
+    seriesField: "year",
+    legend: {
+      position: "top-left",
+    } as any,
+  };
+  return <Bar {...config} />;
+};
 
 class App extends React.Component {
-    constructor(props: any) {
-        super(props);
-        console.log('[p1]', module.hot);
-    }
+  constructor(props: any) {
+    super(props);
+    // console.log('[p1]', module.hot);
+  }
 
-    render() {
-        return (
-            <div className="App" >
-                <img src={img} style={{ width: 100 }} />
-                <h1> This is a building Tool designed by Ethan, Welcome for using. </h1>
-                <h2> The TS Version </h2>
-                <div>
-                    compared with create - react - app, this building tool will expose
-                    webpack.config.js so that you could customize for your project.
-                </div>
-                <div> boy ♂ next ♂ door </div>
-                <div>全给党</div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <div className="flex">
+          <div className="flex-item">
+            <DemoBar />
+            {/* <canvas width={300}/> */}
+          </div>
+          <div className="flex-item">
+            {/* <canvas width={300}/> */}
+            <DemoBar />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;

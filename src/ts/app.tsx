91a1a61efb803c881eb0a3c3f044aca730af08d6
lijiahao <lijiahao@shopee.com>
2022-12-28 14:20:10 +0800

    hooks

--- src/ts/app.tsx
+++ src/ts/app.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import "./app.less";
 import { mult, pow } from "../utils";
 import { Bar } from "@ant-design/plots";
+import { Component, useEffect, useRef, useState } from "react";
 
 // 需要放 index.d.ts 文件到根目录并在 tsconfig
 // includes 里引入才能解决 vscode 报错
@@ -48,28 +49,64 @@ const DemoBar = () => {
   return <Bar {...config} />;
 };
 
-class App extends React.Component {
-  constructor(props: any) {
-    super(props);
-    // console.log('[p1]', module.hot);
-  }
+// let num1 = 0
 
-  render() {
-    return (
-      <div className="App">
-        <div className="flex">
-          <div className="flex-item">
-            <DemoBar />
-            {/* <canvas width={300}/> */}
-          </div>
-          <div className="flex-item">
-            {/* <canvas width={300}/> */}
-            <DemoBar />
-          </div>
-        </div>
-      </div>
-    );
-  }
+function App() {
+  const [num, setNum] = useState(0);
+  const ref = useRef<any>({});
+
+  useEffect(() => {
+    setInterval(() => {
+      const { num } = ref.current;
+      console.log("[p1.1]", { num });
+    }, 1000);
+  }, []);
+
+  ref.current.num = num;
+  // useEffect(() => {
+  //   console.log("each run");
+  //   ref.current.num = num;
+  // });
+
+  const handleClick = () => {
+    setNum(num + 1);
+    console.log("[p1.0]", { num });
+  };
+
+  return (
+    <div>
+      {num}
+      <button onClick={handleClick}>按钮</button>
+    </div>
+  );
 }
+
+// class App extends Component<any, any> {
+//   constructor(props) {
+//     super(props);
+//     this.state = { num: 0 };
+//   }
+//   componentDidMount(): void {
+//     setInterval(() => {
+//       const { num } = this.state;
+//       console.log("[p1.1]", { num });
+//     },1000);
+//   }
+
+//   handleClick = () => {
+//     this.setState({
+//       num: this.state.num + 1,
+//     });
+//   };
+//   render() {
+//     const { num } = this.state;
+//     return (
+//       <div>
+//         {num}
+//         <button onClick={this.handleClick}>click</button>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;

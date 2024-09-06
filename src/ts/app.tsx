17a0a39727930c4469c170209d13966ef40d7720
lijiahao <lijiahao@shopee.com>
2024-09-06 16:31:02 +0800

    react

--- src/ts/app.tsx
+++ src/ts/app.tsx
@@ -1,32 +1,76 @@
 import * as React from "react";
+import { useEffect, useState } from 'react'
 import "./app.less";
 import { mult, pow } from '../utils'
 import axios from 'axios'
+// import 'antd/dist/antd.css';
+import { Button, Input } from 'antd'
+import './app.less'
 
-axios.defaults.withCredentials = true
+const App = () => {
+    const [text, setText] = useState('')
+    const [list, setList] = useState<any[]>([])
 
-// 需要放 index.d.ts 文件到根目录并在 tsconfig
-// includes 里引入才能解决 vscode 报错
-import img from "../assets/react.jpg";
+    useEffect(() => {
+        const liststr = localStorage.getItem('list')
+        if (liststr) {
+            setList(JSON.parse(liststr))
+        }
+    }, [])
 
-class App extends React.Component {
-    constructor(props: any) {
-        super(props);
+    useEffect(() => {
+        console.log('[p1.2] effect')
+        localStorage.setItem('list', JSON.stringify(list))
+    }, [list.length])
+
+    const handleClick = () => {
+        const id = +new Date()
+        list.push({ text, key: id, id })
+        setList([...list])
+        setText('')
     }
 
-    handleClick = () => {
-        console.log("click triggered")
+    const handleChange = (e) => {
+        // console.log('[p1.0] value', value)
+        setText(e.target.value)
     }
 
-    render() {
-        return (
-            <div className="App" >
-                <img src={img} style={{ width: 100 }} />
-                <h1> This is a building Tool designed by Ethan, Welcome for using. </h1>
-                <button onClick={this.handleClick}>fetch</button>
+    const handleDelete = (key) => {
+        console.log('[p1.0] key', key)
+        const index = list.findIndex(e => e.key == key)
+
+        console.log('[p1.0] index', index)
+        list.splice(index, 1)
+        setList([...list])
+    }
+
+    return (
+        <div className="app-container">
+            {/* <h1>304 Caching Example</h1>
+            <img src='http://localhost:3010/api/v1/image'/> */}
+            <div className="top-container">
+                <Input style={{ width: 80 }} value={text} onChange={handleChange} />
+                <Button type='primary' onClick={handleClick}>add</Button>
             </div>
-        );
-    }
+            {list.map(e => {
+                const { text, key } = e
+                return <Item
+                    text={text}
+                    key={key}
+                    id={key}
+                    handleDelete={handleDelete}
+                />
+            })}
+        </div>
+    );
+};
+
+const Item = (props) => {
+    const { text, handleDelete, id } = props;
+    return < div className="item-container" >
+        <div>{text}</div>
+        <Button onClick={() => handleDelete(id)}>delete</Button>
+    </div >
 }
 
 export default App;
